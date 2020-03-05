70cee3debbd09b5726b475e54f087cdd71bad6c9
Pedro Brandão <phbrandao@outlook.com>
2020-03-05 01:40:39 -0300

    Informative text

--- resources/views/member/acoes.blade.php
+++ resources/views/member/acoes.blade.php
@@ -38,6 +38,16 @@
                 {{ session('status') }}
             </div>
             @endif
+
+            <h1>Quais as vantagens de investir em ações?</h1>
+
+            <h5 class="text-justify">Investir em ações é uma opção rentável muito boa para se construir seu patrimônio no longo prazo, já que as empresas, 
+                no longo prazo, tendem a crescer, seguindo o crescimento do país e da economia global. 
+                O mercado ações no Brasil possui uma alta liquidez, ou seja, o investidor tem boa flexibilidade para 
+                tomadas de decisões de entrada e saída do mercado. Investir em ações também pode ser uma ótima opção
+                 para quem deseja viver de renda passiva. Os dividendos, partes do lucro de uma empresa que são 
+                 distribuídos entre os acionistas, são isentos de imposto de renda e também tendem a crescer no longo 
+                 prazo.</h5> <br>
             
             <div class="card-group">
                 <div class="card">
